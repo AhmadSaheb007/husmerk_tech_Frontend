6c01efdb39c87510c2dee38eb22d40ebdd5b4b9e
AhmadSaheb007 <ahmad.saheb007@gmail.com>
2024-02-18 21:36:35 +0600

    updated project section again

--- src/components/Shared/ProjectCard.jsx
+++ src/components/Shared/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import MyBtn from "./MyBtn";
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
   return (
     <div>
       <div className="row">
@@ -10,15 +10,12 @@ const ProjectCard = () => {
             <div className="row">
               <div className="col-lg-12">
                 <img
-                  src="https://images.unsplash.com/photo-1522199670076-2852f80289c3?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src={props.projectImg}
                   alt=""
                   className="img-responsive w-100 rounded-3"
                 />
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipe iure eum
-                  laboriosam similique aliquam et! A, at amet ratione magni fuga
-                  dignissimos ex voluptas, corrupti itaque quos hic eligendi
-                  dolore.
+                  {props.projectDescription}
                 </p>
               </div>
               <div className="col-lg-12">
--- src/components/Shared/ProjectSection.jsx
+++ src/components/Shared/ProjectSection.jsx
@@ -1,52 +1,67 @@
-import React from 'react';
-import '/src/components/Shared/ProjectSection.css'
-import SectionTitle from './SectionTitle';
-import ProjectCard from './ProjectCard';
-
+import React from "react";
+import "/src/components/Shared/ProjectSection.css";
+import SectionTitle from "./SectionTitle";
+import ProjectCard from "./ProjectCard";
+import { useState } from "react";
 const ProjectSection = () => {
+  const [isActive, setIsActive] = useState("false");
+  const scrollDown = () =>{
+    setIsActive(!isActive);
+  };
 
-    function scrollDown(event){
-        document.getElementById('sectionTwo').scrollIntoView()
-    };
-
-    return (
-        <div>
-            <SectionTitle sectionTitle = "Projects" />
-            <div className="container">
-            <div className="scroll-bg py-3 text-center">
-                <div className="scroll-div px-2">
-                    <div className="scroll-object">
-                        <div className="row">
-                            <div className="col-lg-3 py-2">
-                                <ProjectCard/>
-                            </div>
-                            <div className="col-lg-3 py-2">
-                                <ProjectCard/>
-                            </div>
-                            <div className="col-lg-3 py-2">
-                                <ProjectCard/>
-                            </div>
-                            <div className="col-lg-3 py-2" id = 'sectionTwo'>
-                                <ProjectCard/>
-                            </div>
-                            <div className="col-lg-3 py-2">
-                                <ProjectCard/>
-                            </div>
-
-
-                            
-
-
-
-                            
-                        </div>    .
-                    </div>
+  return (
+    <div>
+      <SectionTitle sectionTitle="Projects" />
+      <div className="container">
+        <div className="scroll-bg py-3 text-center">
+          <div className={isActive? "scroll-div" : "make-full-screen"}>
+            <div className="px-2">
+            <div className="scroll-object">
+              <div className="row">
+                <div className="col-lg-3 py-2">
+                  <ProjectCard
+                    projectImg="https://images.unsplash.com/photo-1522199670076-2852f80289c3?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    projectDescription="Lorem ipsum dolor sit amet consectetur adipe iure laboriosam similique aliquam et A, at amet ratione magni fuga dignissimos ex voluptas, corrupti itaque quos hic eligendi dolore."
+                  />
                 </div>
-                <button className='py-2 px-5 bg-black text-light rounded-2 scroll-bg-btn' onClick={scrollDown}>See More</button>
+                <div className="col-lg-3 py-2">
+                  <ProjectCard
+                    projectImg="https://images.unsplash.com/photo-1522199670076-2852f80289c3?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    projectDescription="Lorem ipsum dolor sit amet consectetur adipe iure laboriosam similique aliquam et A, at amet ratione magni fuga dignissimos ex voluptas, corrupti itaque quos hic eligendi dolore."
+                  />
+                </div>
+                <div className="col-lg-3 py-2">
+                  <ProjectCard
+                    projectImg="https://images.unsplash.com/photo-1522199670076-2852f80289c3?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    projectDescription="Lorem ipsum dolor sit amet consectetur adipe iure laboriosam similique aliquam et A, at amet ratione magni fuga dignissimos ex voluptas, corrupti itaque quos hic eligendi dolore."
+                  />
+                </div>
+                <div className="col-lg-3 py-2" id="sectionTwo">
+                  <ProjectCard
+                    projectImg="https://images.unsplash.com/photo-1522199670076-2852f80289c3?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    projectDescription="Lorem ipsum dolor sit amet consectetur adipe iure laboriosam similique aliquam et A, at amet ratione magni fuga dignissimos ex voluptas, corrupti itaque quos hic eligendi dolore."
+                  />
+                </div>
+                <div className="col-lg-3 py-2">
+                  <ProjectCard
+                    projectImg="https://images.unsplash.com/photo-1522199670076-2852f80289c3?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    projectDescription="Lorem ipsum dolor sit amet consectetur adipe iure laboriosam similique aliquam et A, at amet ratione magni fuga dignissimos ex voluptas, corrupti itaque quos hic eligendi dolore."
+                  />
+                </div>
+              </div>{" "}
             </div>
             </div>
+          </div>
+          <button
+            className="py-2 px-5 bg-black text-light rounded-2 scroll-bg-btn"
+            onClick={scrollDown}
+          >
+            {isActive? "See more" : "See Less"}
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ProjectSection;
